9ca4e72d598267b3e1a31602f7f7402c83b66667
surya <ssuryakumar97@gmail.com>
2023-02-10 13:51:14 +0530

    url changed

--- src/service/api.js
+++ src/service/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { API_NOTIFICATION_MESSAGES, SERVICE_URLS } from '../constants/config';
 import { getAccessToken, getType } from '../utils/common.utils';
 
-// const API_URL = 'https://cms-backend-gy29.onrender.com';
-const API_URL = "http://localhost:5000"
+const API_URL = 'https://cms-backend-gy29.onrender.com';
+// const API_URL = "http://localhost:5000"
 
 const axiosInstance = axios.create({
     baseURL : API_URL,
